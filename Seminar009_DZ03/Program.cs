ab201456f7739ded26119aae14ff630cf041a454
Gocha <Frost220v@gmail.com>
2023-06-18 16:47:11 +0500

    программа находит натуральное число по формуле Аккермана

---
+++ Seminar009_DZ03/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 68: Напишите программу 
+// вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.Write("Введите число = ");
+int fistrNum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число = ");
+int lastNum = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(Akkerman(fistrNum, lastNum));
+
+int Akkerman(int num1, int num2)
+{
+    if (num1 == 0)
+    {
+        return num2 + 1;
+    }
+    else if (num2 == 0 && num1 > 0)
+    {
+        return Akkerman(num1 - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(num1 - 1, Akkerman(num1, num2 - 1)));
+    }
+}
