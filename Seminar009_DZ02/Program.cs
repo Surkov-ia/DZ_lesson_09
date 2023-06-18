bee374dcffc5ee0e89e3d7ece441ce9f1739a9fd
Gocha <Frost220v@gmail.com>
2023-06-18 16:44:29 +0500

    программа считает сумму числе в промежутке указанным пользователем

---
+++ Seminar009_DZ02/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 66: Задайте значения M и N.
+//  Напишите программу, которая найдёт сумму 
+//  натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите число = ");
+int fistrNum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число = ");
+int lastNum = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(PrintNum(fistrNum, lastNum) + 1);
+
+int PrintNum(int num1, int num2)
+{
+    int sum = num1;
+
+    if (num1 == num2)
+        return 0;
+    else
+    {
+        num1++;
+        sum = num1 + PrintNum(num1, num2);
+    }
+    // Console.Write($" {sum+1}");
+    return sum;
+}
